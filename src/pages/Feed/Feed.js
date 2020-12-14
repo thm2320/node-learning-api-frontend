b768f915b4cb2d8f363fb4235a2cd891ba9594f2
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-14 11:14:41 +0800

    create formData and upload image for a post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -105,13 +105,20 @@ class Feed extends Component {
     this.setState({
       editLoading: true
     });
-    // Set up data (with image!)
-    let url = 'URL';
+    const formData = new FormData();
+    formData.append('title', postData.title);
+    formData.append('content', postData.content);
+    formData.append('image', postData.image);
+    let url = 'http://localhost:8080/feed/post';
+    let method = 'POST';
     if (this.state.editPost) {
       url = 'URL';
     }
 
-    fetch(url)
+    fetch(url, {
+      method: method,
+      body: formData
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Creating or editing a post failed!');
--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,7 +14,7 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('URL')
+    fetch(`http://localhost:8080/feed/post/${postId}`)
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch status');
@@ -26,7 +26,8 @@ class SinglePost extends Component {
           title: resData.post.title,
           author: resData.post.creator.name,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.post.content
+          content: resData.post.content,
+          image: `http://localhost:8080/${resData.post.imageUrl}`
         });
       })
       .catch(err => {
